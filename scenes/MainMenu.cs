06ddaa459c5d77e5e4534e0abf512521007db3ef
Dylan Tran <killingdyl@yahoo.com>
2025-07-19 13:02:53 -0700

    update menu to start with start button

--- scenes/MainMenu.cs
+++ scenes/MainMenu.cs
@@ -17,7 +17,12 @@ public partial class MainMenu : Control
                 GetNode("PlayerStatuses").GetChild(Players.PlayerIds.Count).GetNode<Label>("Label").Text = $"Player {Players.PlayerIds.Count + 1} Joined";
                 Players.PlayerIds.Add(@event.Device);
                 GetNode<Button>("CenterContainer/Play").Disabled = false;
+                GetNode<Button>("CenterContainer/Play").Text = "Press Start";
             }
+        }
+        if (@event.IsMatch(new InputEventJoypadButton() { Device = -1, ButtonIndex = JoyButton.Start, Pressed = true }))
+        {
+            if (Players.PlayerIds.Count > 0) Play();
         }
     }
 }
