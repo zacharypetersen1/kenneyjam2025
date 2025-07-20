b69c1e75fc09b40418c4496cc6eb15f5d2d352be
Dylan Tran <killingdyl@yahoo.com>
2025-07-19 19:18:39 -0700

    customizable compartment battery location

--- scenes/Compartment.cs
+++ scenes/Compartment.cs
@@ -6,6 +6,9 @@ public partial class Compartment : StaticBody3D
     public Battery Inserted { get; set; }
 
     [Export]
+    public Node3D BatteryLocation;
+
+    [Export]
     public int PowerDraw = 1;
 
     public bool HasPower() => Inserted is not null && Inserted.Charge > 0;
@@ -13,6 +16,9 @@ public partial class Compartment : StaticBody3D
     public void Insert(Battery battery)
     {
         Inserted = battery;
+        battery.GetParent().RemoveChild(battery);
+        BatteryLocation.AddChild(battery);
+        battery.Position = new();
     }
 
     public Battery Remove()
