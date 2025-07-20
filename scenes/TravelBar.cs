43fb3d565cba494c3c64a92112f681c4f0068cb9
Zachary Petersen <zacharypetersen1@gmail.com>
2025-07-19 19:47:44 -0700

    Fix up backgrounds to progress bars

--- scenes/TravelBar.cs
+++ scenes/TravelBar.cs
@@ -3,8 +3,22 @@ using System;
 
 public partial class TravelBar : TextureProgressBar
 {
+    float t;
+    [Export]
+    public Control noThrusterText;
+
     override public void _Process(double delta)
     {
+        t += (float)delta * 5;
         Value = GameManager.inst.CurTravelDist / GameManager.inst.MaxTravelDist;
+
+        if(GameManager.inst.CurTravelSpeed <= 0)
+        {
+            noThrusterText.Visible = Mathf.Sin(t) > 0;
+        }
+        else
+        {
+            noThrusterText.Visible = false;
+        }
     }
 }
